ca13ff597cef26453f99078ba87eee696d37fab8
Rafal Zietak <rzi@vp.pl>
2020-01-05 23:22:31 +0100

    table made by JS r

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,9 @@ exports.signup = function(req, res){
       var fname= post.first_name;
       var lname= post.last_name;
       var mob= post.mob_no;
+      var sesa_no = post.sesa_no;
 
-      var sql = "INSERT INTO `users2`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
+      var sql = "INSERT INTO `elunch_users2`(`sesa_no`,`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + sesa_no + "','" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
 
       var query = db.query(sql, function(err, result) {
 
@@ -32,18 +33,22 @@ exports.login = function(req, res){
       var post  = req.body;
       var name= post.user_name;
       var pass= post.password;
+      var sesa_no =post.sesa_no;
+      console.log(sesa_no);
      
-      var sql="SELECT id, first_name, last_name, user_name FROM `users2` WHERE `user_name`='"+name+"' and password = '"+pass+"'";                           
+      var sql="SELECT id, first_name, last_name, user_name FROM `elunch_users2` WHERE `sesa_no`='"+sesa_no+"' and password = '"+pass+"'";                           
       db.query(sql, function(err, results){      
          if(results.length){
             req.session.userId = results[0].id;
             req.session.first_name = results[0].first_name;
+            req.session.sesa_no = results[0].sesa_no;
             console.log(results[0].id);
             console.log(results[0].first_name);
+            console.log("sesa: ", results[0].sesa_no," tu jest błąd -> do sprawdzeia dlaczego jest undiefined");
             res.redirect('/home/dashboard');
          }
          else{
-            message = 'Złe dane logowania (email lub hasło)';
+            message = 'Złe dane logowania (sesa lub hasło)';
             res.render('index.ejs',{message: message});
          }
                  
@@ -69,7 +74,7 @@ exports.dashboard = function(req, res, next){
       return;
    }
 
-   var sql="SELECT * FROM `users2` WHERE `id`='"+userId+"'";
+   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";
 
    db.query(sql, function(err, results){
       res.render('dashboard.ejs', {fname}); 
@@ -91,7 +96,7 @@ exports.profile = function(req, res){
       return;
    }
 
-   var sql="SELECT * FROM `users2` WHERE `id`='"+userId+"'";          
+   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";          
    db.query(sql, function(err, result){  
       res.render('profile.ejs',{result});
    });
@@ -116,16 +121,18 @@ exports.new_order = function(req, res, next){
    var user =  req.session.user,
    userId = req.session.userId,
    fname= req.session.first_name;
+   sesa_no=req.session.sesa_no;
 
    console.log('userID= '+userId);
    console.log('first_name= '+req.session.first_name);
-   
+   console.log('sesa_no= '+sesa_no);
+
    if(userId == null){
       res.redirect("/login");
       return;
    }
 
-   var sql="SELECT * FROM `users2` WHERE `id`='"+userId+"'";
+   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";
    var dataSet = [
       [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ]
   ];
