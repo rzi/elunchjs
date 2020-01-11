8d41aac0bc3be91092719c3374fbb6ebad837686
Rafal Zietak <rzi@vp.pl>
2020-01-11 22:20:28 +0100

    table in ejs + axios for fetch

--- routes/user.js
+++ routes/user.js
@@ -1,105 +1,115 @@
-
 //---------------------------------------------signup page call------------------------------------------------------
-exports.signup = function(req, res){
-   message = '';
-   if(req.method == "POST"){
-      var post  = req.body;
-      var name= post.user_name;
-      var pass= post.password;
-      var fname= post.first_name;
-      var lname= post.last_name;
-      var mob= post.mob_no;
-      var sesa_no1 = post.sesa_no1;
+exports.signup = function(req, res) {
+  message = "";
+  if (req.method == "POST") {
+    var post = req.body;
+    var name = post.user_name;
+    var pass = post.password;
+    var fname = post.first_name;
+    var lname = post.last_name;
+    var mob = post.mob_no;
+    var sesa_no1 = post.sesa_no1;
 
-      var sql = "INSERT INTO `elunch_users2`(`sesa_no`,`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + sesa_no1 + "','" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
+    var sql =
+      "INSERT INTO `elunch_users2`(`sesa_no`,`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" +
+      sesa_no1 +
+      "','" +
+      fname +
+      "','" +
+      lname +
+      "','" +
+      mob +
+      "','" +
+      name +
+      "','" +
+      pass +
+      "')";
 
-      var query = db.query(sql, function(err, result) {
-
-         message = "Succesfully! Your account has been created.";
-         res.render('signup.ejs',{message: message});
-      });
-
-   } else {
-      res.render('signup');
-   }
+    var query = db.query(sql, function(err, result) {
+      message = "Succesfully! Your account has been created.";
+      res.render("signup.ejs", { message: message });
+    });
+  } else {
+    res.render("signup");
+  }
 };
- 
-//-----------------------------------------------login page call------------------------------------------------------
-exports.login = function(req, res){
-   var message = '';
-   var sess = req.session; 
 
-   if(req.method == "POST"){
-      var post  = req.body;
-      var name= post.user_name;
-      var pass= post.password;
-      var sesa_no1 =post.sesa_no1; //
-      console.log("sesa no1= ", sesa_no1);
-     
-      var sql="SELECT id, first_name, last_name, user_name FROM `elunch_users2` WHERE `sesa_no`='"+sesa_no1+"' and password = '"+pass+"'";                           
-      db.query(sql, function(err, results){      
-         if(results.length){
-            req.session.userId = results[0].id;
-            req.session.first_name = results[0].first_name;
-            req.session.sesa_no1 = sesa_no1;
-            console.log(results[0].id);
-            console.log(results[0].first_name);
-            console.log("sesa: ", sesa_no1);
-            res.redirect('/home/dashboard');
-         }
-         else{
-            message = 'Złe dane logowania (sesa lub hasło)';
-            res.render('index.ejs',{message: message});
-         }
-                 
-      });
-   } else {
-      res.render('index.ejs',{message: message});
-   }
-           
+//-----------------------------------------------login page call------------------------------------------------------
+exports.login = function(req, res) {
+  var message = "";
+  var sess = req.session;
+
+  if (req.method == "POST") {
+    var post = req.body;
+    var name = post.user_name;
+    var pass = post.password;
+    var sesa_no1 = post.sesa_no1; //
+    console.log("sesa no1= ", sesa_no1);
+
+    var sql =
+      "SELECT id, first_name, last_name, user_name FROM `elunch_users2` WHERE `sesa_no`='" +
+      sesa_no1 +
+      "' and password = '" +
+      pass +
+      "'";
+    db.query(sql, function(err, results) {
+      if (results.length) {
+        req.session.userId = results[0].id;
+        req.session.first_name = results[0].first_name;
+        req.session.sesa_no1 = sesa_no1;
+        console.log(results[0].id);
+        console.log(results[0].first_name);
+        console.log("sesa: ", sesa_no1);
+        res.redirect("/home/dashboard");
+      } else {
+        message = "Złe dane logowania (sesa lub hasło)";
+        res.render("index.ejs", { message: message });
+      }
+    });
+  } else {
+    res.render("index.ejs", { message: message });
+  }
 };
 //-----------------------------------------------dashboard page functionality----------------------------------------------
-           
-exports.dashboard = function(req, res, next){
-           
-   var user =  req.session.user,
-   userId = req.session.userId,
-   fname= req.session.first_name;
 
-   console.log('userID= '+userId);
-   console.log('first_name= '+req.session.first_name);
-   
-   if(userId == null){
-      res.redirect("/login");
-      return;
-   }
+exports.dashboard = function(req, res, next) {
+  var user = req.session.user,
+    userId = req.session.userId,
+    fname = req.session.first_name;
 
-   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";
+  console.log("userID= " + userId);
+  console.log("first_name= " + req.session.first_name);
 
-   db.query(sql, function(err, results){
-      res.render('dashboard.ejs', {fname}); 
-      console.log('user= ' + fname)  ;
-   });       
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
+
+  var sql = "SELECT * FROM `elunch_users2` WHERE `id`='" + userId + "'";
+
+  db.query(sql, function(err, results) {
+    res.render("dashboard.ejs", { fname });
+    console.log("user= " + fname);
+  });
 };
 //------------------------------------logout functionality----------------------------------------------
-exports.logout=function(req,res){
-   req.session.destroy(function(err) {
-      res.redirect("/login");
-   })
+exports.logout = function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect("/login");
+  });
 };
 //--------------------------------render user details after login--------------------------------
-exports.profile = function(req, res){
+exports.profile = function(req, res) {
+  var userId = req.session.userId;
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
 
-   var userId = req.session.userId;
-   if(userId == null){
-      res.redirect("/login");
-      return;
-   }
-
-   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";          
-   db.query(sql, function(err, result){  
-      res.render('profile.ejs',{result});
-   });
+  var sql = "SELECT * FROM `elunch_users2` WHERE `id`='" + userId + "'";
+  db.query(sql, function(err, result) {
+    res.render("profile.ejs", { result });
+  });
 };
 //---------------------------------edit users details after login----------------------------------
 // exports.editprofile=function(req,res){
@@ -115,83 +125,91 @@ exports.profile = function(req, res){
 //    });
 // };
 //-----------------------------------------------new_user page functionality----------------------------------------------
-           
-exports.new_order = function(req, res, next){
-           
-   var user =  req.session.user,
-   userId = req.session.userId,
-   fname= req.session.first_name;
-   sesa_no1=req.session.sesa_no1;
 
-   console.log('userID= '+userId);
-   console.log('first_name= '+req.session.first_name);
-   console.log('sesa_n1o= '+sesa_no1);
+exports.new_order = function(req, res, next) {
+  var user = req.session.user,
+    userId = req.session.userId,
+    fname = req.session.first_name,
+    sesa_no1 = req.session.sesa_no1,
+    menu_json;
 
-   if(userId == null){
-      res.redirect("/login");
-      return;
-   }
+  console.log("userID= " + userId);
+  console.log("first_name= " + req.session.first_name);
+  console.log("sesa_n1o= " + sesa_no1);
+  console.log("Method: " +req.method);
+  console.dir(req.query.ID);
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
+  // display menu
+  // var sql="SELECT * FROM `elunch_menu2` WHERE `id`='"+userId+"'";
+  var sql3 = "SELECT * FROM `elunch_menu2` WHERE 1 ";
 
-   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";
-   var dataSet = [
-      [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ]
-  ];
-   // db.query(sql, function(err, results){
-   //    res.render('new_order.ejs', {fname,dataSet}); 
+  db.query(sql3, function(err, results) {
+    menu_json = JSON.stringify(results);
+    console.log("menu_json: ", menu_json[0].id);
+    res.render("new_order.ejs", { fname, menu_json });
+  });
 
-   //     console.log('dataSet', dataSet);
-   // });   
+  // db.query(sql, function(err, results){
+  //    res.render('new_order.ejs', {fname,dataSet});
 
-   var id_order2=1;
-   var sql2="SELECT * FROM `elunch_orders2` WHERE `id`='"+id_order2+"'";
-   db.query(sql2, function(err, results){
-      res.render('new_order.ejs', {fname,results,dataSet}); 
+  //     console.log('dataSet', dataSet);
+  // });
 
-       console.log('dane z bazy', results);
-   });  
- 
+  // var id_order2=1;
+  // var sql2="SELECT * FROM `elunch_orders2` WHERE `id`='"+id_order2+"'";
+  // db.query(sql2, function(err, results){
+  //    res.render('new_order.ejs', {fname,results,dataSet});
+
+  //     console.log('dane z bazy', results);
+  // });
 };
 //-----------------------------------------------new_user page functionality----------------------------------------------
-           
-exports.table = function(req, res, next){
-           
-   var user =  req.session.user,
-   userId = req.session.userId,
-   fname= req.session.first_name;
-   sesa_no1=req.session.sesa_no1;
 
-   console.log('userID= '+userId);
-   console.log('first_name= '+req.session.first_name);
-   console.log('sesa_n1o= '+sesa_no1);
+exports.table = function(req, res, next) {
+  var user = req.session.user,
+    userId = req.session.userId,
+    fname = req.session.first_name;
+  sesa_no1 = req.session.sesa_no1;
 
-   if(userId == null){
-      res.redirect("/login");
-      return;
-   }
+  console.log("userID= " + userId);
+  console.log("first_name= " + req.session.first_name);
+  console.log("sesa_n1o= " + sesa_no1);
 
-   var sql="SELECT * FROM `elunch_users2` WHERE `id`='"+userId+"'";
-   var dataSet = [
-      [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ]
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
+
+  var sql = "SELECT * FROM `elunch_users2` WHERE `id`='" + userId + "'";
+  var dataSet = [
+    [
+      "Tiger Nixon",
+      "System Architect",
+      "Edinburgh",
+      "5421",
+      "2011/04/25",
+      "$320,800"
+    ]
   ];
   var myjson, myjson;
 
-   db.query(sql, function(err, results1){
-      //res.render('new_order.ejs', {fname,dataSet}); 
-      myjson1 = JSON.stringify(results1);
-      
-       console.log('tabela_users: ', myjson1);
-   });   
+  db.query(sql, function(err, results1) {
+    //res.render('new_order.ejs', {fname,dataSet});
+    myjson1 = JSON.stringify(results1);
 
-   var id_order2=1;
-   var sql2="SELECT * FROM `elunch_orders2` WHERE 1"; //`id`='"+id_order2+"'
-   db.query(sql2, function(err, results){
-      // res.send(JSON.stringify({key:"value"}));
-       myjson = JSON.stringify(results);
-      
-      console.log('tabela_zamowien ', myjson);
-      res.render('table.ejs', {myjson1, myjson}); 
-         
-      
-   });  
- 
+    console.log("tabela_users: ", myjson1);
+  });
+
+  var id_order2 = 1;
+  var sql2 = "SELECT * FROM `elunch_orders2` WHERE 1"; //`id`='"+id_order2+"'
+  db.query(sql2, function(err, results) {
+    // res.send(JSON.stringify({key:"value"}));
+    myjson = JSON.stringify(results);
+
+    console.log("tabela_zamowien ", myjson);
+    res.render("table.ejs", { myjson1, myjson });
+  });
 };
