ad5d1fb791f07b6269a5bbfa13da9ddd7fd464cf
rzi <rzi@vp.pl>
2020-01-25 09:30:41 +0100

    pb with time to orders

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ var now = new Date();
 var day = ("0" + now.getDate()).slice(-2);
 var month = ("0" + (now.getMonth() + 1)).slice(-2);
 var today = now.getFullYear() + "-" + month + "-" + day;
-var today = now.getFullYear() + "-" + month + "-" + (day+1);
+// var today = now.getFullYear() + "-" + month + "-" + (day+1);
 var myhour=now.getHours();
 
 var t_currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
@@ -12,22 +12,26 @@ var t_day = t_currentDate.getDate();
 var t_month = ("0"+ (t_currentDate.getMonth() + 1)).slice(-2);
 var t_year = t_currentDate.getFullYear();
 var tommorow =t_year+"-" + t_month+"-" + t_day + "T10:00:00";
-console.log(Date.parse(tommorow)/1000);
+var tommorowTimeStampe=Date.parse(tommorow)/1000;
+console.log("tommorow at 10am: "+ tommorow);
 
 var y_currentDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
 var y_day = y_currentDate.getDate();
 var y_month = ("0"+ (y_currentDate.getMonth() + 1)).slice(-2);
 var y_year = y_currentDate.getFullYear();
 var yesterday =y_year+"-" + y_month+"-" + y_day + "T00:00:01";
-console.log("yesterday "  + Date.parse(yesterday)/1000);
+var yestersayTimeStampe = Date.parse(yesterday)/1000;
+console.log("yesterdayTimeStampe: "  + yestersayTimeStampe);
 
-var endTime;
-if (myhour >9 ){
-  // alert(tommorow);
-  endTime = Date.parse(tommorow);
-  console.log(tommorow);
-//const endTime = now.getFullYear() + "-" + month + "-" + now.getDay();
-}
+var endTime, endTime;
+// if (myhour >9 ){
+//   // alert(tommorow);
+//   endTime = tommorowTimeStampe;
+//   //alert (endTime);
+//   console.log("endTime >9: " + endTime);
+// //const endTime = now.getFullYear() + "-" + month + "-" + now.getDay();
+// }
+
 //const endTime = new Date().getTime();
 // const spanD = document.querySelector('span.d');
 const spanH = document.querySelector('span.h');
@@ -35,19 +39,32 @@ const spanM = document.querySelector('span.m');
 const spanS = document.querySelector('span.s');
 
 setInterval(() => {
- const nowTime = new Date().getTime();
- // const time = Math.floor((endTime - nowTime) / 1000);
- const time = endTime - nowTime;
- const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
-//  console.log(days);
+if (myhour<10){
+  console.log("today: "+today);
+  var todayAt10 =today + "T10:00:00";
+  endTime = Date.parse(todayAt10)/1000;
+  console.log("today (endtime <10): "+ endTime);
+} else{
+  endTime = tommorowTimeStampe
+  console.log("tommorow (endtime  <10): "+ endTime);
+}
 
- let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+nowTime = new Date().getTime();
+nowTime=Math.floor(nowTime/1000);
+ // const time = Math.floor((endTime - nowTime) / 1000);
+ var mytime = endTime - nowTime;
+console.log("endTime: "+endTime);
+console.log("nowTime: "+nowTime);
+//  const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+ console.log("pozostały czas do zamowienia: " + mytime);
+
+ let hours = Math.floor((endTime / ( 60 * 60) - nowTime / ( 60 * 60)) % 24);
  // Przykład - dodanie 0 przeg godziną 
  hours = hours < 10 ? `0${hours}` : hours;
 //  console.log(hours);
- const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+ const minutes = Math.floor((endTime / ( 60) - nowTime / ( 60)) % 60);
 
- const secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+ const secs = Math.floor((endTime  - nowTime ) % 60);
  
 //  spanD.textContent = days;
  spanH.textContent = hours;
